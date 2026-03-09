7d2878540a755f3643390a296de2f46613f3b6f4
Mutsinzi-E <mutsinzienock56@gmail.com>
2026-03-09 12:50:33 +0000

    0x03-debugging project: Betty style fixed and all outputs correct

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,27 +1,38 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * print_remaining_days - prints day of year and remaining days
+ * @month: month number (1-12)
+ * @day: day of month
+ * @year: year
+ *
+ * Description: Calculates the day of the year and the number of
+ * remaining days in the year, taking leap years into account.
+ */
 void print_remaining_days(int month, int day, int year)
 {
-    int leap = 0;
+	int leap = 0;
 
-    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
-        leap = 1;
+	/* Check if leap year */
+	if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+		leap = 1;
 
-    if (leap && month >= 2 && day >= 60)
-        day++;
+	/* Invalid Feb 29 in non-leap year */
+	if (!leap && month == 2 && day == 60)
+	{
+		printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+		return;
+	}
 
-    if (!leap && month == 2 && day == 60)
-    {
-        printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-    }
-    else
-    {
-        printf("Day of the year: %d\n", day);
+	/* If leap year and month after February, add 1 day */
+	if (leap && month > 2)
+		day++;
 
-        if (leap)
-            printf("Remaining days: %d\n", 366 - day);
-        else
-            printf("Remaining days: %d\n", 365 - day);
-    }
+	printf("Day of the year: %d\n", day);
+
+	if (leap)
+		printf("Remaining days: %d\n", 366 - day);
+	else
+		printf("Remaining days: %d\n", 365 - day);
 }
