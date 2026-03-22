710af31806be81325d6bec92792bf6eee80d6020
Mutsinzi-E <mutsinzienock56@gmail.com>
2026-03-22 12:56:19 +0000

    Task 6: Implement print_line function

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,30 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* Prototype for _putchar */
 int _putchar(char c);
-
-/* 0x04: More functions, nested loops */
-
-/* Task 0: Check for uppercase character */
 int _isupper(int c);
-
-/* Task 1: Check for a digit */
 int _isdigit(int c);
-
-/* Task 2: Check for lowercase character */
 int _islower(int c);
-
-/* Task 2: Multiply two integers */
 int mul(int a, int b);
-
-/* Task 3: Print numbers from 0 to 9 */
 void print_numbers(void);
-
-/* Task 4: Print numbers 0 to 9 except 2 and 4 */
 void print_most_numbers(void);
-
-/* Task 5: Print numbers 0 to 14, 10 times */
 void more_numbers(void);
+void print_line(int n);
 
 #endif /* MAIN_H */
