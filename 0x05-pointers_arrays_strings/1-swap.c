13fa40031f2b5b3a8ec279e56b976b8a9100d027
Mutsinzi-E <mutsinzienock56@gmail.com>
2026-03-24 10:35:23 +0000

    Task 1 swap_int complete

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * swap_int - swaps two integers
- * @a: pointer
- * @b: pointer
+ * swap_int - swaps the values of two integers
+ * @a: pointer to first integer
+ * @b: pointer to second integer
  */
 void swap_int(int *a, int *b)
 {
