1b2aca16e8c3133ffa7d0cb37a4c147fac8fb72f
Mutsinzi-E <mutsinzienock56@gmail.com>
2026-03-26 13:02:10 +0000

    Add rev_string function (Betty-compliant)

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: string to reverse
+ */
+void rev_string(char *s)
+{
+	int i = 0, j;
+	char temp;
+
+	/* find length */
+	while (s[i] != '\0')
+		i++;
+
+	/* swap characters */
+	j = i - 1;
+	i = 0;
+	while (i < j)
+	{
+		temp = s[i];
+		s[i] = s[j];
+		s[j] = temp;
+		i++;
+		j--;
+	}
+}
