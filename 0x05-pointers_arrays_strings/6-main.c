15cc3b6194278ce905050eb31ac66995142f39f4
Mutsinzi-E <mutsinzienock56@gmail.com>
2026-03-26 13:52:22 +0000

    Add puts2 function fully Betty-compliant and tested with long string

--- 0x05-pointers_arrays_strings/6-main.c
+++ 0x05-pointers_arrays_strings/6-main.c
@@ -9,7 +9,11 @@ int main(void)
 {
     char *str;
 
-    str = "0123456789";
+    /* Long string for testing puts2 */
+    str = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "
+          "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
+          "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.";
+
     puts2(str);
     return (0);
 }
--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,11 +6,12 @@
  */
 void puts2(char *str)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; str[i] != '\0'; i += 2) /* loop through string */
+	while (str[i] != '\0')
 	{
-		_putchar(str[i]); /* print every other char */
+		_putchar(str[i]);
+		i += 2;
 	}
-	_putchar('\n'); /* always end with newline */
+	_putchar('\n');
 }
