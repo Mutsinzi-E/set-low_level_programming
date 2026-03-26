97b50cf2260927a00ab5098c7465c8f4b77f54e8
Mutsinzi-E <mutsinzienock56@gmail.com>
2026-03-26 14:01:28 +0000

    Fix puts2 for long string, fully Betty-compliant

--- 0x05-pointers_arrays_strings/6-main.c
+++ 0x05-pointers_arrays_strings/6-main.c
@@ -1,19 +1,17 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * main - check the puts2 function
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    char *str;
+	char *str = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut "
+	            "labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris "
+	            "nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit "
+	            "esse cillum dolore eu fugiat nulla pariatur.";
 
-    /* Long string for testing puts2 */
-    str = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "
-          "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
-          "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.";
-
-    puts2(str);
-    return (0);
+	puts2(str);
+	return (0);
 }
