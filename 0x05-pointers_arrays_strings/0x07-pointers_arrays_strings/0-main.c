009d996bc6677c4cdec739e5c547385923f2112d
Mutsinzi-E <mutsinzienock56@gmail.com>
2026-03-26 15:33:21 +0000

    Fix folder structure for auto-grader

--- 0x05-pointers_arrays_strings/0x07-pointers_arrays_strings/0-main.c
+++ 0x05-pointers_arrays_strings/0x07-pointers_arrays_strings/0-main.c
@@ -1,41 +1,11 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 
-/**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
- *
- * Return: Nothing
- */
-void simple_print_buffer(char *buffer, unsigned int size)
-{
-	unsigned int i;
-
-	for (i = 0; i < size; i++)
-	{
-		if (i % 10)
-			printf(" ");
-		if (!(i % 10) && i)
-			printf("\n");
-		printf("0x%02x", buffer[i]);
-	}
-	printf("\n");
-}
-
-/**
- * main - check the code
- *
- * Return: Always 0
- */
 int main(void)
 {
-	char buffer[98] = {0};
+    char buffer[50] = "Talk is cheap. Show me the code.";
+    _memset(buffer, 0x20, 16);
 
-	simple_print_buffer(buffer, 98);
-	_memset(buffer, 0x01, 95);
-	printf("-------------------------------------------------\n");
-	simple_print_buffer(buffer, 98);
-
-	return (0);
+    printf("%s\n", buffer);
+    return 0;
 }
--- 0x05-pointers_arrays_strings/0x07-pointers_arrays_strings/0-memset.c
+++ 0x05-pointers_arrays_strings/0x07-pointers_arrays_strings/0-memset.c
@@ -1,19 +1,11 @@
 #include "main.h"
 
-/**
- * _memset - fills memory with a constant byte
- * @s: pointer to memory area
- * @b: byte to fill
- * @n: number of bytes to fill
- *
- * Return: pointer to memory area s
- */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
+    unsigned int i;
 
-	for (i = 0; i < n; i++)
-		s[i] = b;
+    for (i = 0; i < n; i++)
+        s[i] = b;
 
-	return (s);
+    return s;
 }
