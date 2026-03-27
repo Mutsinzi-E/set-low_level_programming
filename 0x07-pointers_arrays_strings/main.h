9830f7a23018292ec6b09fc6e80c9ea32b1c710f
Mutsinzi-E <mutsinzienock56@gmail.com>
2026-03-27 14:35:25 +0000

    Add Betty-compliant implementations for _strpbrk, _strstr, print_chessboard, and print_diagsums with proper main and header

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,10 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-char *_strstr(char *haystack, char *needle);
-char *_strpbrk(char *s, char *accept);
 unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
 
 void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
 
-#endif
+#endif /* MAIN_H */
