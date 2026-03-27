8f3a7b9b4c81d2e114aea4ac536ad6d1d8918134
Mutsinzi-E <mutsinzienock56@gmail.com>
2026-03-27 11:25:47 +0000

    Add _strspn function and update main.h

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -2,29 +2,23 @@
 
 /**
  * _strspn - gets the length of a prefix substring
- * @s: the string to check
- * @accept: the bytes to match in prefix
+ * @s: string to be scanned
+ * @accept: string containing characters to match
  *
- * Return: number of bytes in initial segment of s consisting only of bytes from accept
+ * Return: number of bytes in the initial segment of s which
+ * consist only of bytes from accept
  */
 unsigned int _strspn(char *s, char *accept)
 {
     unsigned int i = 0, j;
-    int match;
 
     while (s[i])
     {
-        match = 0;
         for (j = 0; accept[j]; j++)
-        {
             if (s[i] == accept[j])
-            {
-                match = 1;
                 break;
-            }
-        }
-        if (!match)
-            break;
+        if (!accept[j])
+            return (i);
         i++;
     }
     return (i);
