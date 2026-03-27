dd18d26ba8f37d713ec9208362ed32db4a0870a6
Mutsinzi-E <mutsinzienock56@gmail.com>
2026-03-27 14:09:09 +0000

    Fix print_chessboard prototype and implementation

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,5 +2,9 @@
 #define MAIN_H
 
 char *_strstr(char *haystack, char *needle);
+char *_strpbrk(char *s, char *accept);
+unsigned int _strspn(char *s, char *accept);
+
+void print_chessboard(char (*a)[8]);
 
 #endif
