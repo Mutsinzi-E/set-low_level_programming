efd9ec436c75b156ca04053a71555028aae4e021
Mutsinzi-E <mutsinzienock56@gmail.com>
2026-03-27 15:05:54 +0000

    Finalize projects, Betty-compliant

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,16 +2,16 @@
 
 /**
  * _strlen - returns the length of a string
- * @s: pointer to the string
+ * @s: input string
  *
- * Return: length of the string
+ * Return: length of string
  */
 int _strlen(char *s)
 {
-    int len = 0;
+	int len = 0;
 
-    while (s[len] != '\0')
-        len++;
+	while (s[len] != '\0')
+		len++;
 
-    return (len);
+	return (len);
 }
