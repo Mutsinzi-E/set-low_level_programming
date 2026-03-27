9b05232b50b47a4600197680f8d17cdf3f7f004c
Mutsinzi-E <mutsinzienock56@gmail.com>
2026-03-27 13:14:04 +0000

    Submit 0x07 project files in correct folder structure

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,23 +1,32 @@
 #include "main.h"
-#include <string.h>
 
 /**
  * _strspn - gets the length of a prefix substring
- * @s: string to check
- * @accept: string containing bytes to match
+ * @s: string to search
+ * @accept: string containing characters to match
  *
- * Return: number of bytes in the initial segment of s which consist
- *         only of bytes from accept
+ * Return: number of bytes in the initial segment of s
+ *         which consist only of bytes from accept
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int count = 0;
+	unsigned int i = 0;
+	int j, found;
 
-	while (*s && strchr(accept, *s))
+	while (s[i])
 	{
-		count++;
-		s++;
+		found = 0;
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				found = 1;
+				break;
+			}
+		}
+		if (!found)
+			break;
+		i++;
 	}
-
-	return (count);
+	return (i);
 }
