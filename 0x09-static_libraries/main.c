609328838b4f997232a926f068fafba74fab2739
Mutsinzi-E <mutsinzienock56@gmail.com>
2026-03-30 15:07:12 +0000

    Final clean static library submission

--- 0x09-static_libraries/main.c
+++
@@ -1,7 +0,0 @@
-#include "main.h"
-
-int main(void)
-{
-    _puts("\"Testing library\"\n");
-    return (0);
-}
--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -3,7 +3,6 @@
 
 int _putchar(char c);
 void _puts(char *str);
-
 int _islower(int c);
 int _isalpha(int c);
 int _abs(int n);
