bd9aec7328ec2ddd30de6a885aae2f1b2231802f
Mutsinzi-E <mutsinzienock56@gmail.com>
2026-03-30 14:53:23 +0000

    fix: add static library functions and headers

---
+++ _atoi.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+int _atoi(char *s)
+{
+    int i = 0, sign = 1, result = 0;
+
+    while (s[i] == ' ')
+        i++;
+
+    if (s[i] == '-')
+        sign = -1;
+
+    while (s[i] == '-' || s[i] == '+')
+        i++;
+
+    while (s[i] >= '0' && s[i] <= '9')
+    {
+        result = result * 10 + (s[i] - '0');
+        i++;
+    }
+
+    return (result * sign);
+}
