47cf5d99d4f230f8e52a790510cb2d0d18112921
Mutsinzi-E <mutsinzienock56@gmail.com>
2026-04-02 12:18:13 +0000

    Fix task 1: argc output and Betty compliance

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 
+/**
+ * main - prints the number of arguments passed to the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
 int main(int argc, char *argv[])
 {
-    (void)argv;
+	(void)argv;
 
-    printf("%d\n", argc);
+	printf("%d\n", argc - 1);
 
-    return (0);
+	return (0);
 }
