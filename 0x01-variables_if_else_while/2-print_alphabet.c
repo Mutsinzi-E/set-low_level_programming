eb9e957a350db93206e32a09967aefac89932c01
Mutsinzi-E <mutsinzienock56@gmail.com>
2026-04-03 12:10:53 +0000

    2-print_alphabet: print lowercase alphabet using putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the alphabet in lowercase
+ *              using only putchar.
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+
+	putchar('\n');
+
+	return (0);
+}
